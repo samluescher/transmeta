8aaa85c0370df7f1763249a742e0cf1c2ffa7aa9
Samuel Luescher <samuel@formlabs.com>
2014-08-06 13:08:45 -0400

    Decoupled from parent repo as its own npm module

--- errors.js
+++ errors.js
@@ -1,20 +1,7 @@
-var util = require('util'),
-	basicErrors = require('../../../errors');
+var createError = require('create-error');
 
-var DataTransformError = function(msg, errors) {
-	DataTransformError.super_.call(this, msg, this.constructor);
-    this.errors = errors;
-}
-util.inherits(DataTransformError, basicErrors.BasicError);
-DataTransformError.prototype.name = 'DataTransformError';
-DataTransformError.prototype.message = 'Transform Error';
-
-var FilterWarning = function(msg, errors) {
-	FilterWarning.super_.call(this, msg, this.constructor);
-}
-util.inherits(FilterWarning, basicErrors.BasicError);
-FilterWarning.prototype.name = 'FilterWarning';
-FilterWarning.prototype.message = 'Value Skipped';
+var DataTransformError = createError('DataTransformError', {message: "Data Transform Error"}),
+	FilterWarning = createError('FilterWarning', {message: "Filter Warning"});
 
 module.exports = {
 	DataTransformError: DataTransformError,
